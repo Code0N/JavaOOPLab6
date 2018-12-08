03f92ec4a9a41a22be9d2c9ade5beee78cd0420f
Code0N <redeemer.of.souls@yandex.ru>
2018-12-08 18:18:56 +0300

    Базовая реализация меню

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,8 @@
 package code0n.lab6
 
 import java.awt.EventQueue
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
 import javax.swing.*
 
 class mainFrame(): JFrame() {
@@ -10,9 +12,23 @@ class mainFrame(): JFrame() {
 
     private fun createUI() {
         title = "Лабораторная работа №6"
+        createMenu()
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setSize(300, 300)
         setLocationRelativeTo(null)
+    }
+
+    private fun createMenu() {
+        val mBar = JMenuBar()
+        val mFile = JMenu("Файл")
+        mFile.mnemonic = KeyEvent.VK_F
+        val eMenuItem = JMenuItem("Создать")
+        eMenuItem.mnemonic = KeyEvent.VK_C
+        eMenuItem.toolTipText = "Создать окно"
+        eMenuItem.addActionListener{ System.exit(0) }
+        mFile.add(eMenuItem)
+        mBar.add(mFile)
+        jMenuBar = mBar
     }
 }
 
